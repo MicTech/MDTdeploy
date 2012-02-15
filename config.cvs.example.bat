2dc93d4027b7629dca6f204bf901ff86cafd8ebd
Michal Tehnik <michal.tehnik@mictech.cz>
2012-02-15 10:06:34 +0100

    Fixed variable name conflict

--- config.cvs.example.bat
+++ config.cvs.example.bat
@@ -1,5 +1,5 @@
 SET cvsUser=user
 SET cvsPass=password
 SET cvsModuleName=module
-SET cvsPath=cvs.server.xx:/disk0/cvsroot/cvspath
-SET CVSROOT=:pserver:%cvsUser%:%cvsPass%@%cvsPath%
+SET cvsServerPath=cvs.server.xx:/disk0/cvsroot/cvspath
+SET CVSROOT=:pserver:%cvsUser%:%cvsPass%@%cvsServerPath%
